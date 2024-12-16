8a9c000d8509d22fb75a6d24f895f5be5e9a6a35
haris shafi <146629838+harisshafi10@users.noreply.github.com>
2024-12-16 19:41:03 +0530

    Update App.jsx
    responsive 2

--- src/App.jsx
+++ src/App.jsx
@@ -29,12 +29,12 @@ const App = () => {
   };
 
   return (
-    <div className="mt-5 rounded-2xl bg-slate-900 w-full max-w-[400px] min-h-[90vh] flex flex-col items-center mx-auto text-slate-300 *:selection:bg-yellow-500">
-      <h1 className="font-bold mb-5 bg-slate-700 rounded-2xl w-full text-center h-10 text-4xl">
+    <div className="mt-5 rounded-2xl bg-slate-900 w-full max-w-[400px] min-h-[90vh] flex flex-col items-center mx-auto text-slate-300 px-4 sm:px-6 *:selection:bg-yellow-500">
+      <h1 className="font-bold mb-5 bg-slate-700 rounded-2xl w-full text-center h-10 text-2xl sm:text-4xl">
         TO-DO LIST
       </h1>
       <TaskForm addTask={addTask} />
-      <div className="h-[68vh] mt-5 w-full bg-slate-800 rounded-md">
+      <div className="h-[60vh] w-full bg-slate-800 rounded-md overflow-y-scroll mt-5">
         <TaskList
           tasks={tasks}
           toggleTaskCompletion={toggleTaskCompletion}
