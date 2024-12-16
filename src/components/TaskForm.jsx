8afd9be1eaa2171c072fe4a97f8fb6095ede76be
harisshafi10 <harisshafi01@gmail.com>
2024-12-16 21:10:55 +0530

    appjs updated

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -23,14 +23,14 @@ const TaskForm = ({ addTask }) => {
       className="flex flex-col sm:flex-row gap-3 items-center w-full"
     >
       <input
-        className="bg-slate-800 rounded-md outline-none p-2 w-full sm:w-auto text-center shadow-slate-700 shadow-md"
+        className="bg-slate-800 rounded-md outline-none p-2 w-full  sm:w-80 md:w-80 lg:w-80 text-center shadow-slate-700 shadow-md"
         type="text"
         placeholder="Enter a Task"
         value={task}
         onChange={inputHandler}
       />
       <button
-        className="p-2 bg-green-700 rounded-md hover:bg-green-800 shadow-slate-700 shadow-md active:scale-[.95] w-full sm:w-auto"
+        className="p-2 bg-green-700 rounded-md hover:bg-green-800 shadow-slate-700 shadow-md active:scale-[.95] w-full sm:w-auto lg:w-20 lg:h-auto lg:p-1"
         type="submit"
       >
         Add Task
